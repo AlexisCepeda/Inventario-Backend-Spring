fc8c9e2d3e745bca315b1644b94625c298065e1e
alexiscepeda <alexiscepedadev@gmail.com>
2025-04-19 12:52:02 -0300

    feat: base version

---
+++ src/main/java/gm/inventarios/repositorio/IProductoRepositorio.java
@@ -0,0 +1,8 @@
+package gm.inventarios.repositorio;
+
+import gm.inventarios.modelo.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductoRepositorio extends JpaRepository<Producto, Integer> {
+
+}
